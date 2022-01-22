ce8b158907cda27e2311ea5b934ccb5007e92cfb
Vladislav <vladway827@gmail.com>
2022-01-22 21:32:14 +0300

    fix actions

--- actions/film.actions.js
+++ actions/film.actions.js
@@ -1,30 +1,34 @@
-import { customAxios } from "../utility/customAxios";
+import {customAxios} from '../utility/customAxios';
 import {createAsyncThunk} from '@reduxjs/toolkit';
 
-export const fetchFilms = createAsyncThunk('fetchFilms', async (_, {rejectWithValue}) => {
-  // console.log('Прием фильмов')
-  try {
+export const fetchFilms = createAsyncThunk(
+  'fetchFilms',
+  async (_, {rejectWithValue}) => {
+    try {
       const response = await customAxios.get('?s=inception&apikey=eaf288a9');
       console.log('fetchFilms ', response);
       return {
         data: response.data.Search,
       };
-  } catch (error) {
-    console.log(error)
-      // logTheEvent('Error', 'fetchFilms', error.response || error);
-      // return rejectWithValue(error.response.data);
-  }
-});
+    } catch (error) {
+      logTheEvent('Error', 'fetchFilms', error.response || error);
+      return rejectWithValue(error.response.data);
+    }
+  },
+);
 
-export const fetchCardFilm = createAsyncThunk('fetchCardVariety', async (id, {rejectWithValue}) => {
-  // console.log('fetchCardVariety ', R.url.variety);
-  try {
+export const fetchCardFilm = createAsyncThunk(
+  'fetchCardVariety',
+  async (id, {rejectWithValue}) => {
+    try {
       const response = await customAxios.get(`?i=${id}&apikey=eaf288a9`);
       console.log('fetchCardFilm ', response);
       return {
         data: response.data,
       };
-  } catch (error) {
-      console.log(error.response.data)
-  }
-});
+    } catch (error) {
+      logTheEvent('Error', 'fetchFilms', error.response || error);
+      return rejectWithValue(error.response.data);
+    }
+  },
+);
